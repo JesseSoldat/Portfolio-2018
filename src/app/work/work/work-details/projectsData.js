62f04cb926a6dbcae4f04be2a0c6aa0c7dc54a20
JLab <jessesoldat@gmail.com>
2018-09-26 09:09:34 -0500

    added warehouse app info

--- src/app/work/work/work-details/projectsData.js
+++ src/app/work/work/work-details/projectsData.js
@@ -1,17 +1,19 @@
 export default {
-  "five-words": {
-    title: "Five Words",
-    img: "fivewords.jpg",
-    url: "https://five-words.surge.sh",
-    github: "https://github.com/5-Words",
+  warehouse: {
+    title: "The Warehouse",
+    img: "warehouse.png",
+    url: "https://jlab-warehouse-app.herokuapp.com",
+    github: "https://github.com/JesseSoldat/warehouse-app-v3",
+    tech: "React, Redux, Node and Mongo",
     description:
-      "Five Words is an app for finding friends based on your most important words.  By limiting the user to just five words we try to push the user to choose their words with great thought and consideration.  What if five words are not enough?  No problem Five Words also offer category searches such as travel, books, food where users can pick five words for each category section and perform more refined searches."
+      "Having trouble keeping track of all of your products?  The Warehouse can help. This application allows you to create products, producers, customers and storage spots. Link products to their storage place either from you computer or print QR Codes to scan two items from any where in your warehouse with a smart phone."
   },
   "around-world": {
     title: "Around the World",
     img: "aroundworld.jpg",
     url: "https://world-view.surge.sh",
     github: "https://github.com/JesseSoldat/Around-The-World-NG2-Firebase",
+    tech: "Angular and Firebase",
     description:
       "Around the world lets you map the places you lived or traveled to and find new friends by searching for others that share these special places.  How big is your world?  Not traveled much?  No worries.  Start with finding stories near your locale.  Want to branch out?  Then just increase the distance of your search as you explore the world one story at a time."
   },
@@ -20,6 +22,7 @@ export default {
     img: "movie-buddy.png",
     url: "https://movie-buddy-71999.firebaseapp.com/movies",
     github: "https://github.com/JesseSoldat/Movie-Buddy-NGRX",
+    tech: "Angular, NgRx and Firebase",
     description:
       "Have you ever had a hard time finding a good movie? If so Movie Buddy can help.  Just begin typing the name and Movie Buddy will start returning results from an extensive database full of movies.  Movie Buddy also offers a powerful search mechanism for displaying other users that also share many of your favorite movies.  From there you can view the movies that they like that are not yet on your list and easily add  them to your collection."
   }
